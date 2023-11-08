6ddb022b9cbc7e2020bc9dbda13ae40d6e075eda
HYUNJOO HAN <hhan39@mtrx-node04pd.dcm.senecacollege.ca>
2023-11-08 12:18:39 -0500

    AidMan Module update

--- milestone/AidMan.cpp
+++ milestone/AidMan.cpp
@@ -1,6 +1,64 @@
+/***********************************************************************
+Final Project Milestone 2
+Name : Hyunjoo Han
+Email : hhan39@myseneca.ca
+ID : 132749227
+Date of completion : 11/02/2023
+
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+***********************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Utils.h"
 #include "AidMan.h"
 
 using namespace std;
 namespace sdds {
+	unsigned int AidMan::menu() const {
+		char* fileName;
+		if (m_fileName == nullptr) {
+			ut.alocpy(fileName, m_fileName);
+		}
+		else {
+			ut.alocpy(fileName, "No file");
+		}
 
+		cout << "Aid Management System Prototype" << endl
+			<< "Date: " << endl
+			<< "Data file: " << fileName << endl
+			<< "---------------------------------" << endl;
+		return m_Menu.run();
+	}
+
+	AidMan::AidMan(const char* fileName) {
+		if (fileName != nullptr) {
+			ut.alocpy(m_fileName, fileName);
+		}
+		else {
+			m_fileName = nullptr;
+		}
+	}
+
+	AidMan::AidMan(const AidMan& aidMan) {
+		*this = aidMan;
+	}
+
+	AidMan& AidMan::operator =(const AidMan& aidMan) {
+		if (this != &aidMan) {
+			ut.alocpy(m_fileName, aidMan.m_fileName);
+		}
+	}
+
+	AidMan::~AidMan() {
+		delete[] m_fileName;
+	}
+
+	void AidMan::run() {
+		while (unsigned int code = menu() != 0) {
+			if (code == 1) {
+				cout << endl << "***List Items***";
+			}
+		}
+	}
 }
--- milestone/AidMan.h
+++ milestone/AidMan.h
@@ -1,7 +1,29 @@
+/***********************************************************************
+Final Project Milestone 2
+Name : Hyunjoo Han
+Email : hhan39@myseneca.ca
+ID : 132749227
+Date of completion : 11/02/2023
+
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+***********************************************************************/
 #ifndef _AIDMAN_H_
 #define _AIDMAN_H_
 
-namespace sdds {
+#include "Menu.h"
 
+namespace sdds {
+	class AidMan {
+		char* m_fileName;
+		Menu m_Menu;
+		unsigned int menu() const;
+	public:
+		AidMan(const char* fileName);
+		AidMan(const AidMan& aidMan);
+		AidMan& operator =(const AidMan& aidMan);
+		~AidMan();
+		void run();
+	};
 }
 #endif
