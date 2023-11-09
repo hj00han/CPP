7d9276a0d729bcfbe9e00dcf345b76411a719a73
HYUNJOO HAN <hhan39@mtrx-node01pd.dcm.senecacollege.ca>
2023-11-09 03:07:25 -0500

    All Moules Update

--- milestone/Status.h
+++ milestone/Status.h
@@ -1,9 +1,9 @@
 /***********************************************************************
-Final Project Milestone 1
+Final Project Milestone 2
 Name : Hyunjoo Han
 Email : hhan39@myseneca.ca
 ID : 132749227
-Date of completion : 11/02/2023
+Date of completion : 11/09/2023
 
 I have done all the coding by myself and only copied the code that
 my professor provided to complete my workshops and assignments.
@@ -22,7 +22,7 @@ namespace sdds {
 		Status& operator =(const char* statDesc);
 		Status& operator =(int statCode);
 		operator int() const;
-		operator const char*() const;
+		operator const char* () const;
 		operator bool() const;
 		Status& clear();
 		friend std::ostream& operator <<(std::ostream& ostr, const Status& status);
