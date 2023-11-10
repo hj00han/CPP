d364bf10757b25c03753f4df133e4bd30a951ed5
HYUNJOO HAN <hhan39@mtrx-node05pd.dcm.senecacollege.ca>
2023-11-09 21:40:22 -0500

    W7 update

--- workshop7/Pack.h
+++ workshop7/Pack.h
@@ -1,6 +1,11 @@
 /***********************************************************************
 Name : Hyunjoo Han
-Email : hunjoo.han25@gmail.com
+Email : hhan39@myseneca.ca
+ID : 132749227
+Date of completion : 11/09/2023
+
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
 ***********************************************************************/
 #ifndef _PACK_H_
 #define _PACK_H_
