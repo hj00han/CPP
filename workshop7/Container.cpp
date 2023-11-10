8d57b4787ede8c7d8daaf6acfec9d23113ac1f92
HYUNJOO HAN <hhan39@mtrx-node04pd.dcm.senecacollege.ca>
2023-11-09 21:25:00 -0500

    W07 update

--- workshop7/Container.cpp
+++ workshop7/Container.cpp
@@ -1,13 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <cstring>
 #include "Container.h"
 
 using namespace std;
 
 namespace sdds {
+	void Container::setEmpty() {
+		m_content[0] = '\0';
+		m_capacity = 0;
+		m_volume = 0;
+	}
+
+	int Container::capacity() const {
+		return m_capacity;
+	}
+
+	int Container::volume() const {
+		return m_volume;
+	}
+
 	Container::Container(const char* content, int capacity, int volume) {
 		if (content == nullptr || volume > capacity) {
-			m_content[0] = '\0';
+			m_content[0] = '\0'; //Invalid statte
 		}
 		else {
 			strcpy(m_content, content);
@@ -23,8 +38,8 @@ namespace sdds {
 			addedVol = volume;
 		}
 		else {
-			m_volume = m_capacity;
 			addedVol = m_capacity - m_volume;
+			m_volume = m_capacity;
 		}
 		return addedVol;
 	}
@@ -36,23 +51,25 @@ namespace sdds {
 			deductedVol = volume;
 		}
 		else {
-			m_volume = 0;
 			deductedVol = m_volume;
+			m_volume = 0;
 		}
 		return deductedVol;
 	}
 	Container::operator bool() const {
-		return m_content[0] != '\0';
+		return !(m_content[0] == '\0'); 
 	}
 	void Container::clear(int capacity, const char* content) {
 		if (capacity > 0 && content != nullptr && content[0] != '\0') {
 			m_capacity = capacity;
 			strcpy(m_content, content);
+			m_volume = 0;
 		}
 	}
-	std::ostream& Container::print(std::ostream& ostr) {
-		if (this) {
-			ostr << m_content <<": (" << m_volume << "cc / " << m_capacity << ")";
+	
+	ostream& Container::print(ostream& ostr) const {
+		if (*this) {
+			ostr << m_content <<": (" << m_volume << "cc/" << m_capacity << ")";
 		}
 		else {
 			ostr << "****: (**cc/***)";
@@ -60,17 +77,44 @@ namespace sdds {
 		return ostr;
 	}
 
-	void Container::setEmpty() {
-		m_content[0] = '\0';
-		m_capacity = 0;
-		m_volume = 0;
+	istream& Container::read(istream& istr) {
+		bool invalidInput = true;
+		int input;
+		int minVal = 1;
+		int maxVal = 220000;
+		if (*this) {
+			cout << "Add to ";
+			cout << *this;
+			cout << endl << "> ";
+			while (invalidInput) {
+				istr >> input;
+				if (!istr) {
+					cout << "Invalid Integer, retry: ";
+				}
+				else if (input < minVal || input > maxVal) {
+					cout << "Value out of range [" << minVal << "<=val<=" << maxVal << "]";
+				}
+				else {
+					invalidInput = false;
+				}
+			}
+			//Add the value when valid input is received
+			cout << "Added " << (*this += input) << " CCs" << endl;
+
+		}
+		else { //When in invalid state
+			cout << "Broken Container, adding aborted! Press <ENTER> to continue...." << endl;
+		}
+		return istr;
 	}
 
-	int Container::capacity() const{
-		return m_capacity;
+	ostream& operator <<(std::ostream& ostr, Container& container) {
+		return container.print(ostr);
 	}
 
-	int Container::volume() const {
-		return m_volume;
+	istream& operator >>(std::istream& istr, Container& container) {
+		return container.read(istr);
 	}
+
+	
 }
--- workshop7/Container.h
+++ workshop7/Container.h
@@ -6,18 +6,20 @@ namespace sdds {
 		char m_content[50] = {};
 		int m_capacity;
 		int m_volume;
-		//bool m_valid;
+	protected:
+		void setEmpty();
+		int capacity() const;
+		int volume() const;
 	public:
 		Container(const char* content, int capacity, int volumne = 0);
 		int operator +=(int volume);
 		int operator -=(int volume);
 		operator bool() const;
 		void clear(int capacity, const char* content);
-		std::ostream& print(std::ostream& ostr);
-	protected:
-		void setEmpty();
-		int capacity() const;
-		int volume() const;
+		std::ostream& print(std::ostream& ostr) const;
+		std::istream& read(std::istream& istr);
+		friend std::ostream& operator <<(std::ostream& ostr, Container& container);
+		friend std::istream& operator >>(std::istream& istr, Container& container);
 	};
 }
 #endif
