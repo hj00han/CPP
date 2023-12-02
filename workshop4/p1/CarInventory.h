79ca61a34bfd2c77397278d881f9297c5412f031
hjoo-han <hyunjoo.han25@gmail.com>
2023-12-02 13:49:33 -0500

    WS4 Update

--- workshop4/p1/CarInventory.h
+++ workshop4/p1/CarInventory.h
@@ -1,12 +1,3 @@
-/***********************************************************************
-Name : Hyunjoo Han
-Email : hhan39@myseneca.ca
-ID : 132749227
-Date of completion : 10/06/2023
-
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-***********************************************************************/
 #ifndef SDDS_CarInventory_H 
 #define SDDS_CarInventory_H
 namespace sdds {
--- workshop4/p2/Label.h
+++ workshop4/p2/Label.h
@@ -1,13 +1,3 @@
-/***********************************************************************
-Name : Hyunjoo Han
-Email : hhan39@myseneca.ca
-ID : 132749227
-Date of completion : 10/09/2023
-
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-***********************************************************************/
-
 #ifndef SDDS_Label_H 
 #define SDDS_Label_H
 #define MAX_TEXT 70
