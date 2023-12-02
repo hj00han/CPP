c8851becc4baf9e4abb4735acd9c962b7a2486ef
hjoo-han <hyunjoo.han25@gmail.com>
2023-12-02 13:45:50 -0500

    workshop update

--- workshop2/p1/File.h
+++ workshop2/p1/File.h
@@ -1,25 +1,3 @@
-/***********************************************************************
-Name : Hyunjoo Han
-Email : hhan39@myseneca.ca
-ID : 132749227
-Date of completion : 9/19/2023
-
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-***********************************************************************/
-
-/***********************************************************************
-// OOP244 Workshop #2 lab (part 1)
-//
-// File  File.h
-// Version 1.0
-// Author   Fardad Soleimanloo
-// Description
-//    To be completed by students
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-***********************************************************************/
 #ifndef SDDS_FILE_H_
 #define SDDS_FILE_H_
 namespace sdds {
--- workshop2/p2/Population.h
+++ workshop2/p2/Population.h
@@ -1,26 +1,4 @@
-/***********************************************************************
-Name : Hyunjoo Han
-Email : hhan39@myseneca.ca
-ID : 132749227
-Date of completion : 9/26/2023
-
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-***********************************************************************/
-
-/***********************************************************************
-// OOP244 Workshop #2 DIY (part 2):
-//
-// File  Population.h
-// Version 1.0
-// Author   Fardad Soleimanloo
-// Description
-//   To completed by students
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-***********************************************************************/#ifndef SDDS_POPULATION_H_
-
+#ifndef SDDS_POPULATION_H_
 #define SDDS_POPULATION_H_
 
 namespace sdds {
