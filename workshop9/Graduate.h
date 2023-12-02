dc498ce6a0a5aa43364827902ac784a3a84014ba
hjoo-han <hyunjoo.han25@gmail.com>
2023-12-02 13:53:51 -0500

    WS9 Update

--- workshop9/Graduate.h
+++ workshop9/Graduate.h
@@ -1,12 +1,3 @@
-/***********************************************************************
-Name : Hyunjoo Han
-Email : hhan39@myseneca.ca
-ID : 132749227
-Date of completion : 11/22/2023
-
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-***********************************************************************/
 #ifndef _GRADUATE_H_
 #define _GRADUATE_H_
 #include "Student.h"
